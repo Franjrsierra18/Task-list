6f0cde34cbb2788404ed4f5aa637338cc2081019
Francisco Sierra <franjrsierra18@gmail.com>
2019-08-17 14:08:25 +0200

    Local Storage arreglado para los botones

--- app.js
+++ app.js
@@ -7,10 +7,34 @@ const filter = document.querySelector('#filter');
 loadAllEventListeners();
 
 function loadAllEventListeners() {
+  document.addEventListener('DOMContentLoaded', getTasks)
   form.addEventListener('submit', addTask);
   taskList.addEventListener('click', removeItem);
   clearBtn.addEventListener('click', clearTasks);
   filter.addEventListener('keyup', filterTasks);
+}
+
+function getTasks(e) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function (task) {
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(task));
+  
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+  
+    li.appendChild(link);
+  
+    taskList.appendChild(li);
+  })
 }
 
 function addTask(e) {
@@ -52,18 +76,40 @@ function removeItem(e) {
   if(e.target.parentElement.classList.contains('delete-item')){
     if(confirm('Are you sure??')){
     e.target.parentElement.parentElement.remove();
+    removeFromLocalStorage(e.target.parentElement.parentElement);
     }
   };
   // e.preventDefault();
 }
 
-function clearTasks(e) {
+function removeFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.forEach(function (task, i) {
+    if (taskItem.textContent === task) {
+      tasks.splice(i,1);
+    }
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function clearTasks() {
   //taskList.innerHTML = '';
   if (confirm('Are you sure??')) {
     while (taskList.firstChild) {
       taskList.removeChild(taskList.firstChild);
+      // removeFromLocalStorage(taskList.firstChild);
     }
   }
+  clearLS();
+}
+
+function clearLS() {
+  localStorage.clear();
 }
 
 function filterTasks(e) {
